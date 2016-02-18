c1a4286520475fce69986324587bce643e50f526
Евгений Елчев <yolchev@gmail.com>
2016-02-18 23:15:15 +0700

    Initial Commit

---
+++ Ble/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ble
+//
+//  Created by Евгений Елчев on 18.02.16.
+//  Copyright © 2016 JonFir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Ble/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Ble
+//
+//  Created by Евгений Елчев on 18.02.16.
+//  Copyright © 2016 JonFir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
